23f75aa05426e099c3fcbf1a748191332ec33253
Jason Chen <jason316497@gmail.com>
2026-06-12 14:59:25 +0800

    Add nvim config

---
+++ nvim/lua/core/options.lua
@@ -0,0 +1,12 @@
+-- Tab
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4              -- Shift 4 spaces when tab
+vim.opt.expandtab = true            -- Use spaces instead of tabs
+
+-- UI
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = "line"
+vim.opt.splitbelow = true           -- open new vertical split bottom
+vim.opt.splitright = true           -- open new horizontal splits right
+vim.opt.showmode = false            -- disable "-- INSERT --" mode hint
+vim.opt.wrap = true
